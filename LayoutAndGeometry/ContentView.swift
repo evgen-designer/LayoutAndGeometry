93c8ffaa19ce250c4c2d447502505f30c0aa0318
Ievgen Kuznietsov <ievgen.contact@gmail.com>
2024-12-09 15:23:53 +0000

    Make views near the top of the scroll view fade out to 0 opacity – I would suggest starting at about 200 points from the top.

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -20,11 +20,23 @@ struct ContentView: View {
                             .frame(maxWidth: .infinity)
                             .background(colors[index % 7])
                             .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(calculateOpacity(proxy: proxy, fullView: fullView))
                     }
                     .frame(height: 40)
                 }
             }
         }
+    }
+    
+    func calculateOpacity(proxy: GeometryProxy, fullView: GeometryProxy) -> Double {
+        let minY = proxy.frame(in: .global).minY
+        let fadeStart: CGFloat = 200 // Starts fading from 200 points from the top
+        let fadeRange: CGFloat = 100 // Fade completely over 100 points
+        
+        if minY < fadeStart {
+            return max(0, 1 - (fadeStart - minY) / fadeRange)
+        }
+        return 1
     }
 }
 
