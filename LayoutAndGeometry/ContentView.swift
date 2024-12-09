53245760cbc2da79675c473e1d51a6b0f433a771
Ievgen Kuznietsov <ievgen.contact@gmail.com>
2024-12-09 15:25:48 +0000

    Make views adjust their scale depending on their vertical position, with views near the bottom being large and views near the top being small. I would suggest going no smaller than 50% of the regular size.

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -21,6 +21,7 @@ struct ContentView: View {
                             .background(colors[index % 7])
                             .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
                             .opacity(calculateOpacity(proxy: proxy, fullView: fullView))
+                            .scaleEffect(calculateScale(proxy: proxy, fullView: fullView))
                     }
                     .frame(height: 40)
                 }
@@ -30,13 +31,22 @@ struct ContentView: View {
     
     func calculateOpacity(proxy: GeometryProxy, fullView: GeometryProxy) -> Double {
         let minY = proxy.frame(in: .global).minY
-        let fadeStart: CGFloat = 200 // Starts fading from 200 points from the top
-        let fadeRange: CGFloat = 100 // Fade completely over 100 points
+        let fadeStart: CGFloat = 200
+        let fadeRange: CGFloat = 100
         
         if minY < fadeStart {
             return max(0, 1 - (fadeStart - minY) / fadeRange)
         }
         return 1
+    }
+    
+    func calculateScale(proxy: GeometryProxy, fullView: GeometryProxy) -> CGFloat {
+        let minY = proxy.frame(in: .global).minY
+        let viewHeight = fullView.size.height
+        
+        // Minimum scale of 0.5 (50%), maximum of 1.0 (100%)
+        let scale = 1.0 - min(max(0, (viewHeight - minY) / viewHeight), 0.5)
+        return max(0.5, scale)
     }
 }
 
