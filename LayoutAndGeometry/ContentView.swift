f6d4d2ed06fde5233f5b0aff752bd854ec9bcd88
Ievgen Kuznietsov <ievgen.contact@gmail.com>
2024-12-09 15:37:44 +0000

    naming fix

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -12,17 +12,17 @@ struct ContentView: View {
         GeometryReader { fullView in
             ScrollView {
                 ForEach(0..<50) { index in
-                    GeometryReader { geo in
+                    GeometryReader { proxy in
                         Text("Row #\(index)")
                             .font(.title)
                             .frame(maxWidth: .infinity)
-                            .background(Color(hue: geo.frame(in: .global).minY / fullView.size.height,
+                            .background(Color(hue: proxy.frame(in: .global).minY / fullView.size.height,
                                                saturation: 1,
                                                brightness: 1))
-                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5,
+                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5,
                                               axis: (x: 0, y: 1, z: 0))
-                            .opacity(max(0, min(geo.frame(in: .global).minY / 200, 1)))
-                            .scaleEffect(max(geo.frame(in: .global).minY / fullView.size.height + 0.5, 0.5))
+                            .opacity(max(0, min(proxy.frame(in: .global).minY / 200, 1)))
+                            .scaleEffect(max(proxy.frame(in: .global).minY / fullView.size.height + 0.5, 0.5))
                     }
                     .frame(height: 40)
                 }
